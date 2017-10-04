ca814c28c12a3a086a3d5bdcb2228136ebd68020
Sergi Hervas <32094995+sergihervas@users.noreply.github.com>
2017-10-04 16:26:01 +0200

    Create asymptoticMK.R

---
+++ asymptoticMK.R
@@ -0,0 +1,65 @@
+## only fits exponential model, depends on fitMKmodel and predictNLS
+## has no watchdog, it does not check data, no error handling yet
+## returns only a table with results, no plot
+
+asymptoticMK_final <- function(x, y, xlow=0, xhigh=1) {
+  
+  ## loading required packages
+  require(MASS, quietly = TRUE)
+  require(nls2, quietly = TRUE)
+   
+  ## assign proper names to the columns of x and y 
+  names(x) <- c("daf", "pN", "pS")
+  names(y) <- c("Chr",  "Pop",  "m0f",  "D0f",  "m4f",  "D4f")
+  
+  ## parse the data from argument x
+  f <- x[[1]] #derived alelle frequencies
+  p <- x[[2]] #non-synonymous polymorphism 
+  p0 <- x[[3]] #synonymous polymorphism
+  
+  ## parse the data from argument y
+  m <- as.numeric(y$m0f) #number of non-synonymous analyzed positions   
+  m0 <- as.numeric(y$m4f) ##number of synonymous analyzed positions
+  d <- as.numeric(y$D0f) #non-synonymous divergence
+  d0 <- as.numeric(y$D4f) #synonymous divergence
+  # pop <- y$Pop #population name, used for plotting...
+  
+  ## compute alpha values and trim
+  alpha <- 1 - (d0/d) * (p/p0)
+  cutoff_f1 <- xlow
+  cutoff_f2 <- xhigh
+  trim <- ((f >= cutoff_f1) & (f <= cutoff_f2))
+  f_trimmed <- f[trim]
+  alpha_trimmed <- alpha[trim]
+  
+  ## compute the original MK alpha
+  alpha_nonasymp <- 1 - (d0/d) * (sum(p[trim])/sum(p0[trim])) #using trimmed values
+      
+  ## two-step nls2() model fit at a given level of precision (res)
+  mod1 <- fitMKmodel(alpha_trimmed, f_trimmed, 10)
+  
+  ## if mod1 did not work, try a deeper scan for a decent fit (res=20)
+  if (length(mod1) == 0) {
+    mod1 <- fitMKmodel(alpha_trimmed, f_trimmed, 20)
+  } 
+  
+  tryCatch({
+    mod2 <- lm(alpha_trimmed ~ f_trimmed)
+  }, error=function(cond) {})
+  
+  ## compute confidence intervals of alpha using predictNLS 
+  ci_pred <- predictNLS(mod1, newdata=data.frame(f_trimmed=1.0))
+  alpha_1_low <- ci_pred[6]
+  alpha_1_high <- ci_pred[7]
+  
+  ## preparation of ouput (alpha asym, a, b, c)
+  alpha_1_est <- predict(mod1, newdata=data.frame(f_trimmed=1.0))
+  const_a <- coef(mod1)["const_a"]
+  const_b <- coef(mod1)["const_b"]
+  const_c <- coef(mod1)["const_c"]
+  
+  ## output table
+  result_df <- data.frame(model="exponential", a=const_a, b=const_b, c=const_c, alpha_asymptotic=alpha_1_est, CI_low=ci_pred[6], ci_high=ci_pred[7], alpha_original=alpha_nonasymp, row.names=NULL)
+  return(result_df)
+}
+
