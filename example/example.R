6e3b869b612a3a3ac8d37549a5ccb7a412ded24a
Sergi Hervas <32094995+sergihervas@users.noreply.github.com>
2017-10-13 18:01:42 +0200

    Update example.R

--- example/example.R
+++ example/example.R
@@ -1,25 +1,19 @@
 ### example procedure to execute the package.
 
 ## 1st. load all functions ##
-
 #(...)
 
-
 ## 2nd. read data ##
-
-#setwd("")
+#setwd("...")
 x <- read.table("RAL_Chr2L.txt", header=T) #polymorphism file (DAF)
 y <- read.table("RAL_Chr2L_div.txt", header=T) #divergence and m file
 
-
 ## 3rd. perform analysis ##
-
 w <- iMK(x, y, 0, 1)
 w #check out results
+plotDAF(x) #plots
+plotIMK(w$iMK)
+plotALPHA(x, y, w$asymptoticMK, 0, 1)
 
 #check watchdog:
 # w <- iMK(x, y, 0, 1.4)
-
-# plot results
-plot1(w$iMK)
-plot2(x, y, w$asymptoticMK, 0, 1)
