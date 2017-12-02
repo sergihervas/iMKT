1da6cd1db4a419493ebc463c24fc9ad3668f2181
jmurga <murgamoreno@gmail.com>
2017-12-02 18:46:55 +0100

    List argument included

--- R/multiple_datasets.R
+++ R/multiple_datasets.R
@@ -22,41 +22,71 @@
 #' @export
 #' 
 
-multiple_datasets<-function(directory="Directory",test="MKT type"){
-  wd<-directory
-  setwd(wd)
-  files<-list.files(wd)
-  subset_daf<-list.files(wd,pattern = "daf.*$")
-  subset_divergence<-list.files(wd,pattern = "divergence.*$")
-  names<-sub("*.daf.*", "", subset_daf)
+multiple_datasets<-function(directory="Directory",test="mkt type",fullanalyis=TRUE/FALSE,idlist="listfile"){
+  
+  wd<-directory;setwd(wd) #SET AND CREATE A WORKING DIRECTORY
+  files<-list.files(wd) #LIST ALL FILES IN DIRECTORY
+  subset_daf<-list.files(wd,pattern = "daf.*$")  #SEPARATE FILES IN DAF AND DIVERGENCE
+  subset_divergence<-list.files(wd,pattern = "divergence.*$")  #SEPARATE FILES IN DAF AND DIVERGENCE
 
-  if(test=="standard"){
-    for (i in names){
-      daf<-(grep(paste0(i,".daf"),subset_daf,value=T))
-      divergence<-grep(paste0(i,".divergence"),subset_divergence,value = T)
-      z<-read.table(daf,header = T)
-      c<-read.table(divergence,header = T)
-      mkt_standard(z,c)}
+  result<-list() #EMPTY RESULT
+  
+  if (fullanalyis==FALSE & idlist!='NA'){
+    list<-as.matrix(read.table(idlist))
+    if(test=="standard"){
+      for (i in list){
+        daf<-(grep(paste0(i,".daf"),subset_daf,value=T))
+        divergence<-(grep(paste0(i,".divergence"),subset_divergence,value = T))
+        z<-read.table(daf,header = T)
+        c<-read.table(divergence,header = T)
+        # result<-append(result,)
+        temp<-list("ID"=i,"StandarMKT"=mkt_standard(z,c))
+        result<-append(result,temp)
+      }
+    }
+    return(result)
   }
-  else if(test=="FWW"){
-    for (i in names){
-      daf<-(grep(paste0(i,".daf"),subset_daf,value=T))
-      divergence<-grep(paste0(i,".divergence"),subset_divergence,value = T)
+else{
+  names<-sub("*.daf.*", "", subset_daf)
+  if(test=="standard"){
+    for (i in 1:length(names)){
+      daf<-(grep(paste0(names[i],".daf"),subset_daf,value=T))
+      divergence<-(grep(paste0(names[i],".divergence"),subset_divergence,value = T))
       z<-read.table(daf,header = T)
       c<-read.table(divergence,header = T)
-      mkt_standard(z,c)}
+      # result<-append(result,)
+      temp<-list("ID"=names[i],"StandarMKT"=mkt_standard(z,c))
+      result<-append(result,temp)
+    }
+  }
+  
+  else if(test=="FWW"){
+    for (i in 1:length(names)){
+      daf<-(grep(paste0(names[i],".daf"),subset_daf,value=T))
+      divergence<-(grep(paste0(names[i],".divergence"),subset_divergence,value = T))
+      z<-read.table(daf,header = T)
+      c<-read.table(divergence,header = T)
+      # result<-append(result,)
+      temp<-list("ID"=i,"StandarMKT"=mkt_fww(z,c))
+      result<-append(result,temp)
+    }
   }
   else if(test=="DGRP"){
-    for (i in names){
-      daf<-(grep(paste0(i,".daf"),subset_daf,value=T))
-      divergence<-grep(paste0(i,".divergence"),subset_divergence,value = T)
+    for (i in 1:length(names)){
+      daf<-(grep(paste0(names[i],".daf"),subset_daf,value=T))
+      divergence<-(grep(paste0(names[i],".divergence"),subset_divergence,value = T))
       z<-read.table(daf,header = T)
       c<-read.table(divergence,header = T)
-      mkt_standard(z,c)}
+      # result<-append(result,)
+      temp<-list("ID"=names[i],"StandarMKT"=mkt_DGRP(z,c))
+      result<-append(result,temp)
+    }
   }
+  
 }
+  return(result)
+ }
 
-
-
-
-
+  
+c<-multiple_datasets(directory = "~/Test",test = "standard",fullanalyis = TRUE, idlist = "NA")
+c
