3e4c0c3f494050d5c68e886188e8599a06cf04dd
Marta Coronado <marta.coronado@uab.cat>
2017-12-03 20:15:56 +0100

    Minor changes

--- R/iMK.R
+++ R/iMK.R
@@ -99,7 +99,7 @@ iMK <- function(daf, divergence, xlow, xhigh, seed) {
   # Fraction of f, b and d sites
   fraction <-data.frame(Fraction=c(d,f,b), Type = c("d","f","b"), MKT = rep("Asymptotic MK", 3))
   
-  plotfraction <- ggplot(fraction) + geom_bar(stat = "identity",aes_string(x = "MKT", y = "Fraction", fill = "Type")) + coord_flip() + theme_Publication() + ylab(label = "Fraction") + scale_fill_manual(values = c("#386cb0","#fdb462","#7fc97f","#ef3b2c","#662506","#a6cee3","#fb9a99","#984ea3","#ffff33"), breaks=c("d","f","b"), labels=c(expression(italic("d")), expression(italic("f")),expression(italic("b")))) + theme(axis.title.y=element_blank(), axis.ticks.y=element_blank(), axis.text.y=element_blank(), panel.border = element_rect(colour = "black", fill=NA, size=1))  +  scale_y_discrete(limit=seq(0,1,0.25), expand = c(0, 0))
+  plotfraction <- ggplot(fraction) + geom_bar(stat = "identity",aes_string(x = "MKT", y = "Fraction", fill = "Type")) + coord_flip() + theme_Publication() + ylab(label = "Fraction") + scale_fill_manual(values = c("#386cb0","#fdb462","#7fc97f","#ef3b2c","#662506","#a6cee3","#fb9a99","#984ea3","#ffff33"), breaks=c("d","f","b"), labels=c(expression(italic("d")), expression(italic("f")),expression(italic("b")))) + theme(axis.title.y=element_blank(), axis.ticks.y=element_blank(), axis.text.y=element_blank(),   axis.line = element_blank(), panel.border = element_rect(colour = "black", fill=NA, size=1))  +  scale_y_discrete(limit=seq(0,1,0.25), expand = c(0, 0))
   plotfraction
   
   # DAF graph
