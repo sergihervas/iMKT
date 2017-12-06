4799d58893ec438e9ac1c0900809063ee5c21921
jmurga <murgamoreno@gmail.com>
2017-12-06 16:10:53 +0100

    check_input dafs categories allows over 10

--- R/check_input.R
+++ R/check_input.R
@@ -1,6 +1,6 @@
 #' Error handling to compute correctly each MKT type. ¿mix between Messer & Haller? Preguntar Sergi
 #' Date = 30/11/2016
-#'' Author = Sergi Hervas, Jesús Murga
+#' Author = Sergi Hervas, Jesús Murga
 #'
 #'
 #' @param daf dad file
@@ -33,6 +33,10 @@ check_input <- function(daf, divergence, xlow, xhigh){
         dataIsGood <- FALSE
         error <- "Argument daf contains no data rows or it is malformed. Check the header!"
         mainErrors<-append(mainErrors,error) }
+    if(NROW(daf)<= 10) { #I think we should allow other DAF categories!! maybe force at least 10? # I'd check that the minimum # of rows are 10, but then not to force to be 20 or 50, only check if the DAF frequency ranges from 0.1 to 0.9, for example
+     dataIsGood <- FALSE
+     error <- "Daf categories are less than 10. Check the header or change you input!"
+     mainErrors <- append(mainErrors,error) }
     # if(NROW(daf) != 20 & NROW(daf) != 10) { #I think we should allow other DAF categories!! maybe force at least 10? # I'd check that the minimum # of rows are 10, but then not to force to be 20 or 50, only check if the DAF frequency ranges from 0.1 to 0.9, for example
         #  dataIsGood <- FALSE
         #  error <- "Daf categories in daf are not correct define. Check the header!"
