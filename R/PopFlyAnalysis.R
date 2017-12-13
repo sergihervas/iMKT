af0428c7aec6ddb9cb76096efe906f018d73c872
sergi <sergi.hervas@uab.cat>
2017-12-13 20:21:30 +0100

    PopFlyAnalysis function

--- R/PopFlyAnalysis.R
+++ R/PopFlyAnalysis.R
@@ -1,10 +1,9 @@
-#' @title subsetPopData
+#' @title PopFlyAnalysis
 #'
-#' @description Perform iMK using a subset of PopFly or PopHuman data defined by custom genes and populations lists 
+#' @description Perform any MK test using a subset of PopFly data defined by custom genes and populations lists 
 #'
 #' @details put details here
 #'
-#' @param data input PopFly or PopHuman data
 #' @param genes list of genes
 #' @param pops list of populations
 #' @param recomb group genes according to recombination values (must specify number of bins). TRUE/FALSE
@@ -16,49 +15,35 @@
 #' @return None
 #'
 #' @examples
-#' ## Load PopFly data
+#' ## Load PopFly data into your workspace
 #' # loadPopFly()
 #' ## Perform analysis
 #' # mygenes <- c("FBgn0053196", "FBgn0000008")
-#' # subsetPopData("PopFly", mygenes , c("RAL","ZI"), recomb=F)
+#' # PopFlyAnalysis(mygenes , c("RAL","ZI"), recomb=F)
+#' # mygenes <- 
+#' # mygenes <- c("FBgn0000008","FBgn0000014","FBgn0000015","FBgn0000017","FBgn0000018","FBgn0000022",
+#' #              "FBgn0000024","FBgn0000028","FBgn0000032","FBgn0000036","FBgn0000037","FBgn0000038",
+#' #              "FBgn0000039","FBgn0000042","FBgn0000043","FBgn0000044","FBgn0000045","FBgn0000046")
+#' # PopFlyAnalysis(mygenes , c("RAL","ZI"), recomb=T, bins=3, test="asymptotic", xlow=0, xhigh=0.9)
 #'
 #' @import utils
 #' @import stats
 #'
 #' @export
 
-## test data 
-## gen <- c("FBgn0000008","FBgn0000014","FBgn0000015","FBgn0000017","FBgn0000018","FBgn0000022",
-##          "FBgn0000024","FBgn0000028","FBgn0000032","FBgn0000036","FBgn0000037","FBgn0000038",
-##          "FBgn0000039","FBgn0000042","FBgn0000043","FBgn0000044","FBgn0000045","FBgn0000046")
-## a <- subsetPopData("PopFly", gen , c("RAL","ZI"), recomb=F, bins=3, test="DGRP")
-## b <- subsetPopData("PopFly", gen , c("RAL","ZI"), recomb=T, bins=3)
-## subsetPopData("PopFly", gen , c("RAL","ZI"), recomb=T, bins=3, test="FWW")
-## subsetPopData("PopFly", gen , c("RAL","ZI"), recomb=T, bins=3, test="foo")
-## subsetPopData("PopFly", gen , c("RAL","ZI"), recomb=T, bins=3, test="DGRP", 0, 0.9)
-## subsetPopData("PopFly", gen , c("RAL","ZI"), recomb=T, bins=3, test="asymptotic", xlow=0, xhigh=0.9)
-## subsetPopData("PopFly", gen , c("RAL","ZI"), recomb=F, test="asymptotic", xlow=0, xhigh=0.9)
+PopFlyAnalysis <- function(genes=c("gene1","gene2","..."), pops=c("pop1","pop2","..."), recomb=TRUE/FALSE, bins=0, test=c("standard","DGRP","FWW","asymptotic","iMK"), xlow=0, xhigh=1) { 
+  
+  ## Get PopFly data
+  if (exists("PopFlyData") == TRUE) {
+    data <- get("PopFlyData")
+  } else {
+    stop("Load PopFly data, with the command loadPopFly(), before using this function.") }
 
-subsetPopData <- function(data=c("PopFly","PopHuman"), genes=c("gene1","gene2","..."), pops=c("pop1","pop2","..."), recomb=TRUE/FALSE, bins=0, test=c("standard","DGRP","FWW","asymptotic","iMK"), xlow=0, xhigh=1) { 
-  
-  ## Get PopFly or PopHuman data
-  if (data == "PopFly"){
-    if (exists("PopFlyData") == TRUE) {
-      data <- get("PopFlyData")
-    } else {
-      stop("Load PopFly data, with the command loadPopFly(), before using this function.") }}
-  
-  else if (data == "PopHuman"){
-    if (exists("PopHumanData") == TRUE) {
-      data <- get("PopHumanData")
-    } else {
-      stop("Load PopHuman data, with the command loadPopHuman(), before using this function.") }}
-  
   ## Check input variables
   ## Numer of arguments
   if (nargs() < 4 && nargs()) {
     stop("You must specify 4 arguments at least: data, genes, pops, recomb (T/F).\nIf test = asymptotic or test = iMK, you must specify xlow and xhigh values.") }
-
+  
   ## Argument genes
   if (length(genes) == 0 || genes == "" || !is.character(genes)) {
     stop("You must specify at least one gene.") }
@@ -67,7 +52,7 @@ subsetPopData <- function(data=c("PopFly","PopHuman"), genes=c("gene1","gene2","
     difGenes <- paste(difGenes, collapse=", ")
     stopMssg <- paste0("MKT data is not available for the requested gene(s).\nRemember to use FlyBase IDs (FBgn...)\nThe genes that caused the error are: ", difGenes, ".")
     stop(stopMssg) }
-
+  
   ## Argument pops
   if (length(pops) == 0 || pops == "" || !is.character(pops)) {
     stop("You must specify at least one population.") }
@@ -77,11 +62,11 @@ subsetPopData <- function(data=c("PopFly","PopHuman"), genes=c("gene1","gene2","
     difPops <- paste(difPops, collapse=", ")
     stopMssg <- paste0("MKT data is not available for the sequested populations(s).\nSelect among the following populations:\nAM, AUS, CHB, EA, EF, EG, ENA, EQA, FR, RAL, SA, SD, SP, USI, USW, ZI.\nThe populations that caused the error are: ", difPops, ".")
     stop(stopMssg) }
-
+  
   ## Argument recomb
   if (recomb != TRUE && recomb != FALSE) {
     stop("Parameter recomb must be TRUE or FALSE.") }
-
+  
   ## Argument bins
   if (recomb == TRUE) {
     if (nargs() != 5 && nargs()!= 6 && nargs() != 8 || !is.numeric(bins) || bins == 0  || bins == 1) {
@@ -91,13 +76,13 @@ subsetPopData <- function(data=c("PopFly","PopHuman"), genes=c("gene1","gene2","
   }
   if (recomb == FALSE && bins != 0) {
     warning("Parameter bins not used! (recomb=F selected)") }
-
+  
   ## Argument test and xlow + xhigh (when necessary)
   if(missing(test)) {
     test <- "standard"
   }
   else if (test != "standard" && test != "DGRP" && test != "FWW" && test != "asymptotic" && test != "iMK") {
-     stop("Parameter test must be one of the following: standard, DGRP, FWW, asymptotic, iMK")
+    stop("Parameter test must be one of the following: standard, DGRP, FWW, asymptotic, iMK")
   }
   if (length(test) > 1) {
     stop("Select only one of the following tests to perform: standard, DGRP, FWW, asymptotic, iMK") }
@@ -129,7 +114,7 @@ subsetPopData <- function(data=c("PopFly","PopHuman"), genes=c("gene1","gene2","
       ## create bins
       binsize <- round(nrow(x)/bins) ## Number of genes for each bin
       count <- 1; dat <- NULL
-
+      
       for (i in 0:nrow(x)) {
         if (i%%binsize == 0) { ## Only if reminder of division = 0 (equally sized bins)
           i1 <- i + binsize - 1
@@ -161,58 +146,58 @@ subsetPopData <- function(data=c("PopFly","PopHuman"), genes=c("gene1","gene2","
         f <- seq(0.05,0.95,0.1)
         mi <- 0; m0 <- 0
         Di <- 0; D0 <- 0
-
+        
         ## Group genes
         x1 <- droplevels(x1)
         for (l in levels(x1$Name)) {
           x2 <- x1[x1$Name == l, ]
-
+          
           ## DAF
           x2$DAF0f <- as.character(x2$DAF0f); x2$DAF4f <- as.character(x2$DAF4f)
           daf0f <- unlist(strsplit(x2$DAF0f, split=";"))
           daf4f <- unlist(strsplit(x2$DAF4f, split=";"))
           daf0f <- as.numeric(daf0f); daf4f <- as.numeric(daf4f)
           Pi <- Pi + daf0f; P0 <- P0 + daf4f
-
+          
           ## Divergence
           mi <- mi + x2$mi; m0 <- m0 + x2$m0
           Di <- Di + x2$di; D0 <- D0 + x2$d0
-       }
-
-      ## Proper formats
-      daf <- cbind(f, Pi, P0); daf <- as.data.frame(daf)
-      names(daf) <- c("daf","Pi","P0")
-      div <- cbind(mi, Di, m0, D0); div <- as.data.frame(div)
-      names(div) <- c("mi","Di","m0","D0")
-
-      ## Check data
-      check <- check_input(daf, div, xlow, xhigh)
-      if(check$data == FALSE) {
-        stop(check$print_errors) }
+        }
+        
+        ## Proper formats
+        daf <- cbind(f, Pi, P0); daf <- as.data.frame(daf)
+        names(daf) <- c("daf","Pi","P0")
+        div <- cbind(mi, Di, m0, D0); div <- as.data.frame(div)
+        names(div) <- c("mi","Di","m0","D0")
+        
+        ## Check data
+        check <- check_input(daf, div, xlow, xhigh)
+        if(check$data == FALSE) {
+          stop(check$print_errors) }
+        
+        ## Perform test
+        if(test == "standard") {
+          output <- standard(daf, div)
+        }
+        else if(test == "DGRP") {
+          output <- DGRP(daf, div)
+        }
+        else if(test == "FWW") {
+          output <- FWW(daf,div)
+        }
+        else if(test == "asymptotic") {
+          output <- asymptoticMK(daf, div, xlow, xhigh)
+        }
+        else if(test == "iMK") {
+          output <- iMK(daf, div, xlow, xhigh)
+        }
+        
+        ## Fill list with each bin
+        outputListBins[[paste("Recombination bin = ",j)]] <- output
+      }
       
-      ## Perform test
-      if(test == "standard") {
-        output <- standard(daf, div)
-      }
-      else if(test == "DGRP") {
-        output <- DGRP(daf, div)
-      }
-      else if(test == "FWW") {
-        output <- FWW(daf,div)
-      }
-      else if(test == "asymptotic") {
-        output <- asymptoticMK(daf, div, xlow, xhigh)
-      }
-      else if(test == "iMK") {
-        output <- iMK(daf, div, xlow, xhigh)
-      }
-      
-      ## Fill list with each bin
-      outputListBins[[paste("Recombination bin = ",j)]] <- output
-    }
-    
-    ## Fill list with each pop
-    outputList[[paste("Population = ",k)]] <- outputListBins
+      ## Fill list with each pop
+      outputList[[paste("Population = ",k)]] <- outputListBins
     }
     
     ## Warning if some genes are lost. Bins must be equally sized.
@@ -223,11 +208,11 @@ subsetPopData <- function(data=c("PopFly","PopHuman"), genes=c("gene1","gene2","
       warningMssg <- paste0("The ",missingGenes," gene(s) with highest recombination rate estimates (", genesNames, ") was/were excluded from the analysis in order to get equally sized bins.\n")
       warning(warningMssg)
     }
-  
+    
     ## Return output
     return(outputList)
   }
-
+  
   ## If NO recombination analysis selected
   else if (recomb == FALSE) {
     
@@ -247,14 +232,14 @@ subsetPopData <- function(data=c("PopFly","PopHuman"), genes=c("gene1","gene2","
       ## Group genes
       for (j in levels(x$Name)) {
         x1 <- x[x$Name == j, ]
-
+        
         ## DAF
         x1$DAF0f <- as.character(x1$DAF0f); x1$DAF4f <- as.character(x1$DAF4f)
         daf0f <- unlist(strsplit(x1$DAF0f, split=";"))
         daf4f <- unlist(strsplit(x1$DAF4f, split=";"))
         daf0f <- as.numeric(daf0f); daf4f <- as.numeric(daf4f)
         Pi <- Pi + daf0f; P0 <- P0 + daf4f
-
+        
         ## Divergence
         mi <- mi + x1$mi; m0 <- m0 + x1$m0
         Di <- Di + x1$di; D0 <- D0 + x1$d0
