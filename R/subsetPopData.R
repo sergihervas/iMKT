642f3319b1ec617c74fa820a04cdeeb05765fe68
sergi <sergi.hervas@uab.cat>
2017-12-13 19:08:49 +0100

    subsetPopData done, should test

--- R/subsetPopData.R
+++ R/subsetPopData.R
@@ -20,34 +20,24 @@
 #' # loadPopFly()
 #' ## Perform analysis
 #' # mygenes <- c("FBgn0053196", "FBgn0000008")
-#' # subsetPopData("PopFly", mygenes , c("RAL","ZI"), recomb=F, bins=0, xlow=0, xhigh=0.9, test="standard")
+#' # subsetPopData("PopFly", mygenes , c("RAL","ZI"), recomb=F)
 #'
 #' @import utils
 #' @import stats
 #'
 #' @export
 
-#######################
-### IN CONSTRUCTION ###
-#######################
-
 ## test data 
-#gen <- c("FBgn0000008","FBgn0000014","FBgn0000015","FBgn0000017","FBgn0000018","FBgn0000022","FBgn0000024","FBgn0000028","FBgn0000032","FBgn0000036","FBgn0000037","FBgn0000038","FBgn0000039","FBgn0000042","FBgn0000043","FBgn0000044","FBgn0000045","FBgn0000046","FBgn0000047","FBgn0000052","FBgn0000053","FBgn0000054","FBgn0000055","FBgn0000056","FBgn0000057","FBgn0000061","FBgn0000063","FBgn0000064","FBgn0000071","FBgn0000075","FBgn0000077","FBgn0000078","FBgn0000079","FBgn0000083","FBgn0000084","FBgn0000092","FBgn0000094","FBgn0000097","FBgn0000099","FBgn0000100","FBgn0000108","FBgn0000109","FBgn0000114","FBgn0000115","FBgn0000116","FBgn0000117","FBgn0000119","FBgn0000120","FBgn0000121","FBgn0000137","FBgn0000139","FBgn0000140","FBgn0000146","FBgn0000147","FBgn0000150","FBgn0000152","FBgn0000153","FBgn0000157","FBgn0000158","FBgn0000163","FBgn0000166","FBgn0000173","FBgn0000179","FBgn0000180","FBgn0000181","FBgn0000182","FBgn0000183","FBgn0000206","FBgn0000210","FBgn0000212","FBgn0000216","FBgn0000221","FBgn0000227","FBgn0000228","FBgn0000229","FBgn0000233","FBgn0000239","FBgn0000241","FBgn0000244","FBgn0000246","FBgn0000247","FBgn0000250","FBgn0000251","FBgn0000253","FBgn0000256","FBgn0000257","FBgn0000259","FBgn0000261","FBgn0000273","FBgn0000274","FBgn0000276","FBgn0000277","FBgn0000278","FBgn0000279","FBgn0000283","FBgn0000286","FBgn0000287","FBgn0000289","FBgn0000299","FBgn0000303")
-#subsetPopData("PopFly", gen , c("RAL","ZI"), recomb=F, bins=3, test="DGRP")
-#subsetPopData("PopFly", gen , c("RAL","ZI"), recomb=T, bins=3)
-#subsetPopData("PopFly", gen , c("RAL","ZI"), recomb=T, bins=3, test="DGRP")
-#subsetPopData("PopFly", gen , c("RAL","ZI"), recomb=T, bins=3, test="foo")
-#subsetPopData("PopFly", gen , c("RAL","ZI"), recomb=T, bins=3, test="DGRP", 0, 0.9)
-#subsetPopData("PopFly", gen , c("RAL","ZI"), recomb=T, bins=3, test="asymptotic", xlow=0, xhigh=0.9)
+## gen <- c("FBgn0000008","FBgn0000014","FBgn0000015","FBgn0000017","FBgn0000018","FBgn0000022","FBgn0000024","FBgn0000028","FBgn0000032","FBgn0000036","FBgn0000037","FBgn0000038","FBgn0000039","FBgn0000042","FBgn0000043","FBgn0000044","FBgn0000045","FBgn0000046","FBgn0000047","FBgn0000052","FBgn0000053","FBgn0000054","FBgn0000055","FBgn0000056","FBgn0000057","FBgn0000061","FBgn0000063","FBgn0000064","FBgn0000071","FBgn0000075","FBgn0000077","FBgn0000078","FBgn0000079","FBgn0000083","FBgn0000084","FBgn0000092","FBgn0000094","FBgn0000097","FBgn0000099","FBgn0000100","FBgn0000108","FBgn0000109","FBgn0000114","FBgn0000115","FBgn0000116","FBgn0000117","FBgn0000119","FBgn0000120","FBgn0000121","FBgn0000137","FBgn0000139","FBgn0000140","FBgn0000146","FBgn0000147","FBgn0000150","FBgn0000152","FBgn0000153","FBgn0000157","FBgn0000158","FBgn0000163","FBgn0000166","FBgn0000173","FBgn0000179","FBgn0000180","FBgn0000181","FBgn0000182","FBgn0000183","FBgn0000206","FBgn0000210","FBgn0000212","FBgn0000216","FBgn0000221","FBgn0000227","FBgn0000228","FBgn0000229","FBgn0000233","FBgn0000239","FBgn0000241","FBgn0000244","FBgn0000246","FBgn0000247","FBgn0000250","FBgn0000251","FBgn0000253","FBgn0000256","FBgn0000257","FBgn0000259","FBgn0000261","FBgn0000273","FBgn0000274","FBgn0000276","FBgn0000277","FBgn0000278","FBgn0000279","FBgn0000283","FBgn0000286","FBgn0000287","FBgn0000289","FBgn0000299","FBgn0000303")
+## a <- subsetPopData("PopFly", gen , c("RAL","ZI"), recomb=F, bins=3, test="DGRP")
+## b <- subsetPopData("PopFly", gen , c("RAL","ZI"), recomb=T, bins=3)
+## subsetPopData("PopFly", gen , c("RAL","ZI"), recomb=T, bins=3, test="FWW")
+## subsetPopData("PopFly", gen , c("RAL","ZI"), recomb=T, bins=3, test="foo")
+## subsetPopData("PopFly", gen , c("RAL","ZI"), recomb=T, bins=3, test="DGRP", 0, 0.9)
+## subsetPopData("PopFly", gen , c("RAL","ZI"), recomb=T, bins=3, test="asymptotic", xlow=0, xhigh=0.9)
+## subsetPopData("PopFly", gen , c("RAL","ZI"), recomb=F, test="asymptotic", xlow=0, xhigh=0.9)
 
-## To Do:
-  ## if predictNLS fails, error handling
-  ## think how to report / return --> list by pop?
-  ## which test to perform?
-
-subsetPopData <- function(data=c("PopFly","PopHuman"), genes=c("gene1","gene2","..."), 
-                          pops=c("pop1","pop2","..."), recomb=TRUE/FALSE, bins=0, 
-                          test=c("standard","DGRP","FWW","asymptotic","iMK"), xlow=0, xhigh=1) { 
+subsetPopData <- function(data=c("PopFly","PopHuman"), genes=c("gene1","gene2","..."), pops=c("pop1","pop2","..."), recomb=TRUE/FALSE, bins=0, test=c("standard","DGRP","FWW","asymptotic","iMK"), xlow=0, xhigh=1) { 
   
   ## Get PopFly or PopHuman data
   if (data == "PopFly"){
@@ -64,8 +54,8 @@ subsetPopData <- function(data=c("PopFly","PopHuman"), genes=c("gene1","gene2","
   
   ## Check input variables
   ## Numer of arguments
-  if (nargs() != 4 && nargs() != 5 && nargs()!= 6 && nargs() != 8) {
-    stop("You must specify 4 arguments at least: data, genes, pops, recomb (T/F).\nIf test = asymptotic or test = iMK, you must specify xlow and xhihg values.") }
+  if (nargs() < 4 && nargs()) {
+    stop("You must specify 4 arguments at least: data, genes, pops, recomb (T/F).\nIf test = asymptotic or test = iMK, you must specify xlow and xhigh values.") }
 
   ## Argument genes
   if (length(genes) == 0 || genes == "" || !is.character(genes)) {
@@ -88,8 +78,7 @@ subsetPopData <- function(data=c("PopFly","PopHuman"), genes=c("gene1","gene2","
 
   ## Argument recomb
   if (recomb != TRUE && recomb != FALSE) {
-    stop("Parameter recomb must be TRUE or FALSE.")
-  }
+    stop("Parameter recomb must be TRUE or FALSE.") }
 
   ## Argument bins
   if (recomb == TRUE) {
@@ -99,8 +88,7 @@ subsetPopData <- function(data=c("PopFly","PopHuman"), genes=c("gene1","gene2","
       stop("Parameter bins > (genes/2). At least 2 genes for each bin are required.") }
   }
   if (recomb == FALSE && bins != 0) {
-    warning("Parameter bins not used! (recomb=F selected)")
-  }
+    warning("Parameter bins not used! (recomb=F selected)") }
 
   ## Argument test and xlow + xhigh (when necessary)
   if(missing(test)) {
@@ -109,11 +97,11 @@ subsetPopData <- function(data=c("PopFly","PopHuman"), genes=c("gene1","gene2","
   else if (test != "standard" && test != "DGRP" && test != "FWW" && test != "asymptotic" && test != "iMK") {
      stop("Parameter test must be one of the following: standard, DGRP, FWW, asymptotic, iMK")
   }
-  print(test)
+  if (length(test) > 1) {
+    stop("Select only one of the following tests to perform: standard, DGRP, FWW, asymptotic, iMK") }
   if ((test == "standard" || test == "DGRP" || test == "FWW") && (xlow != 0 || xhigh != 1)) {
     warningMssgTest <- paste0("Parameters xlow and xhigh not used! (test = ",test," selected)")
-    warning(warningMssgTest)
-  }
+    warning(warningMssgTest) }
   
   ## Arguments xlow, xhigh features (numeric, bounds...) checked in check_input()
   
@@ -121,12 +109,18 @@ subsetPopData <- function(data=c("PopFly","PopHuman"), genes=c("gene1","gene2","
   subsetGenes <- data[(data$Name %in% genes & data$Pop %in% pops), ]
   subsetGenes$Name <- as.factor(subsetGenes$Name)
   subsetGenes <- droplevels(subsetGenes)
-
+  
   ## If recomb analysis is selected
   if (recomb == TRUE) {
     
+    ## Declare output list (each element 1 pop)
+    outputList <- list()
+    
     for (k in levels(subsetGenes$Pop)) {
-      print(k)
+      
+      ## Declare bins output list (each element 1 bin)
+      outputListBins <- list()
+      
       x <- subsetGenes[subsetGenes$Pop == k, ]
       x <- x[order(x$p_bp), ]
       
@@ -157,7 +151,6 @@ subsetPopData <- function(data=c("PopFly","PopHuman"), genes=c("gene1","gene2","
       
       ## Iterate through each recomb bin
       for (j in levels(dat$Group)) {
-        print(j)
         x1 <- dat[dat$Group == j, ]
         
         ## Set counters to 0
@@ -195,9 +188,29 @@ subsetPopData <- function(data=c("PopFly","PopHuman"), genes=c("gene1","gene2","
       if(check$data == FALSE) {
         stop(check$print_errors) }
       
-      ## Perform iMK
-      out <- iMK(daf, div, xlow, xhigh)
+      ## Perform test
+      if(test == "standard") {
+        output <- standard(daf, div)
       }
+      else if(test == "DGRP") {
+        output <- DGRP(daf, div)
+      }
+      else if(test == "FWW") {
+        output <- FWW(daf,div)
+      }
+      else if(test == "asymptotic") {
+        output <- asymptoticMK(daf, div, xlow, xhigh)
+      }
+      else if(test == "iMK") {
+        output <- iMK(daf, div, xlow, xhigh)
+      }
+      
+      ## Fill list with each bin
+      outputListBins[[paste("Recombination bin = ",j)]] <- output
+    }
+    
+    ## Fill list with each pop
+    outputList[[paste("Population = ",k)]] <- outputListBins
     }
     
     ## Warning if some genes are lost. Bins must be equally sized.
@@ -208,13 +221,18 @@ subsetPopData <- function(data=c("PopFly","PopHuman"), genes=c("gene1","gene2","
       warningMssg <- paste0("The ",missingGenes," gene(s) with highest recombination rate estimates (", genesNames, ") was/were excluded from the analysis in order to get equally sized bins.\n")
       warning(warningMssg)
     }
+  
+    ## Return output
+    return(outputList)
   }
 
   ## If NO recombination analysis selected
   else if (recomb == FALSE) {
-
+    
+    ## Declare output list (each element 1 pop)
+    outputList <- list()
+    
     for (i in levels(subsetGenes$Pop)) {
-      print(i)
       x <- subsetGenes[subsetGenes$Pop == i, ]
       
       ## Set counters to 0
@@ -251,9 +269,29 @@ subsetPopData <- function(data=c("PopFly","PopHuman"), genes=c("gene1","gene2","
       if(check$data == FALSE) {
         stop(check$print_errors) }
       
-      ## Perform iMK 
-      out <- iMK(daf, div, xlow, xhigh)
+      ## Perform test
+      if(test == "standard") {
+        output <- standard(daf, div)
+      }
+      else if(test == "DGRP") {
+        output <- DGRP(daf, div)
+      }
+      else if(test == "FWW") {
+        output <- FWW(daf,div)
+      }
+      else if(test == "asymptotic") {
+        output <- asymptoticMK(daf, div, xlow, xhigh)
+      }
+      else if(test == "iMK") {
+        output <- iMK(daf, div, xlow, xhigh)
+      }
+      
+      ## Fill list with each pop
+      outputList[[paste("Population = ",i)]] <- output
     }
+    
+    ## Return output
+    return(outputList)
   }
 }
 
