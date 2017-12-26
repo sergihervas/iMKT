e2778890ad0dc72b4116b5adfec67417666abfca
sergi <sergi.hervas@uab.cat>
2017-12-26 20:51:20 +0100

    update iMKT pipeline vignette

--- inst/doc/iMKTPipeline.R
+++ inst/doc/iMKTPipeline.R
@@ -10,7 +10,8 @@ knitr::opts_chunk$set(
 
 ## ----load package and see sample data, echo=T----------------------------
 ## Load package
-#devtools::install_github("sergihervas/iMKT")
+# install.packages("devtools")
+# devtools::install_github("sergihervas/iMKT")
 library(iMKT)
 
 ## Sample daf data
@@ -25,12 +26,6 @@ standard(daf=mydafdata, divergence=mydivergencedata)
 ## ----FWW, echo=TRUE------------------------------------------------------
 FWW(daf=mydafdata, divergence=mydivergencedata)
 
-## ----Result FWW, echo=F, eval=F------------------------------------------
-#  methodFWW<-FWW(daf = mydafdata,divergence = mydivergencedata)
-#  methodFWW$Results
-#  methodFWW$`MKT tables`
-#  methodFWW$`Divergence metrics`
-
 ## ----FWW plot, echo=TRUE, fig.width=6, fig.height=4----------------------
 FWW(daf=mydafdata, divergence=mydivergencedata, list_cutoff=c(0.05, 0.15,0.25,0.35), plot=TRUE)
 
@@ -38,14 +33,11 @@ FWW(daf=mydafdata, divergence=mydivergencedata, list_cutoff=c(0.05, 0.15,0.25,0.
 DGRP(daf=mydafdata, divergence=mydivergencedata)
 
 ## ---- echo=TRUE, fig.width=6, fig.height=6-------------------------------
-DGRP(daf=mydafdata, divergence=mydivergencedata, list_cutoff=c(0.05,0.15,0.25), plot=TRUE)
+DGRP(daf=mydafdata, divergence=mydivergencedata, list_cutoff=c(0.05, 0.15,0.25,0.35), plot=TRUE)
 
 ## ----Asymptotic MKT, echo=TRUE-------------------------------------------
 asymptoticMK(daf=mydafdata, divergence=mydivergencedata, xlow=0, xhigh=0.9)
 
 ## ----iMK, echo=TRUE, fig.width=6, fig.height=9---------------------------
 iMK(daf=mydafdata, divergence=mydivergencedata, xlow=0, xhigh=0.9, plot=TRUE)
-
-## ----multipleDatasets, eval=FALSE, include=FALSE-------------------------
-#  # multipleDatasets(directory = directory, test = c("standard", "DGRP", "FWW","asymptotic", "iMK"), xlow = 0, xhigh = 1, fullAnalysis = TRUE/FALSE,idList = "NA")
 
