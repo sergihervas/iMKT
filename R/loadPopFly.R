170e0ac7b3595c81f4f8afcf7fcc0012f5205d28
Jesús Murga Moreno <murgamoreno@gmail.com>
2019-03-18 11:20:16 +0100

    Update loadPopFly.R

--- R/loadPopFly.R
+++ R/loadPopFly.R
@@ -18,6 +18,6 @@
 loadPopFly <- function() {
   PopFlyData <- ""
   cat("Loading PopFly data into your workspace.\nThis process may take several seconds to complete, please be patient.\n")
-  x <- read.table("http://popfly.uab.cat/files/genes/GenesData_recomb_comeron.tab", header=T)
+  x <- read.table("http://popfly.uab.cat/files/genes/GenesData_recomb_comeron.tab", header=T, sep='\t')
   PopFlyData <<- x
 }
